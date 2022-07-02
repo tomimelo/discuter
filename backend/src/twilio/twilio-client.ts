361e564f79389e5f3c74595900150319b8dfb56e
Tomas Melone <tomasmelone@hotmail.com>
2022-07-02 03:58:41 +0200

    refactor twilio client

--- backend/src/twilio/twilio-client.ts
+++ backend/src/twilio/twilio-client.ts
@@ -1,22 +1,22 @@
 import twilio from 'twilio'
 import AccessToken from 'twilio/lib/jwt/AccessToken'
-
-interface TwilioConfig {
-  accountSid: string;
-  apiKey: string;
-  apiSecret: string;
-  serviceSid: string;
-}
+import { TwilioConfig } from './twilio-config'
 
 export class TwilioClient {
   public constructor (private readonly config: TwilioConfig) {}
+
+  public getAccessTokenWithGrant (identity: string): string {
+    const accessToken = this.getAccessToken(identity)
+    accessToken.addGrant(this.getConversationGrant())
+    return accessToken.toJwt()
+  }
 
   private getAccessToken (identity: string): AccessToken {
     const { AccessToken } = twilio.jwt
     return new AccessToken(this.config.accountSid, this.config.apiKey, this.config.apiSecret, { identity })
   }
 
-  private getConversationGrant (identity: string): AccessToken.ChatGrant {
+  private getConversationGrant (): AccessToken.ChatGrant {
     const { ChatGrant } = twilio.jwt.AccessToken
     return new ChatGrant({
       serviceSid: this.config.serviceSid
