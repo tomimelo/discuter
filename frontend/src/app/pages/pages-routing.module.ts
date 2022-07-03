78ebe377bfb8a8e6f317a94141eb2adc956ba8a4
Tomas Melone <tomasmelone@hotmail.com>
2022-07-03 02:11:38 +0200

    ref room routing

--- frontend/src/app/pages/pages-routing.module.ts
+++ frontend/src/app/pages/pages-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../guards/auth.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: "home", loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
-  {path: "room", loadChildren: () => import('./room/room.module').then(m => m.RoomModule)},
-  {path: "room/:id", loadChildren: () => import('./room-child/room-child.module').then(m => m.RoomChildModule)},
+  {path: "room/:id", canActivate: [AuthGuard], loadChildren: () => import('./room/room.module').then(m => m.RoomModule)},
   {path: '**', redirectTo: 'home'}
 ];
 
