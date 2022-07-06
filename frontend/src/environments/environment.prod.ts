21bfb0c308863deba85a1c8ba5e2e4f31f3def64
Tomas Melone <60193559+tomimelo@users.noreply.github.com>
2022-07-06 08:28:39 +0000

    add prod api url

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://qsewkuaclihjlpterjeq.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzZXdrdWFjbGloamxwdGVyamVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY3MTc0MzMsImV4cCI6MTk3MjI5MzQzM30.Y842Aa06USUjaq9oIDZ0JuQE453aOb96OdkIUgCgO8E',
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://discuter-production.up.railway.app/api'
 };
