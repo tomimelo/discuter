55f6e30525a8397c7ca013472e5c9450cf272a7e
Tomas Melone <60193559+tomimelo@users.noreply.github.com>
2022-07-07 10:57:19 +0000

    join conversation when accesing through url

--- frontend/src/app/pages/room/room.component.ts
+++ frontend/src/app/pages/room/room.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Conversation } from '@twilio/conversations';
 import { TwilioService } from 'src/app/services/twilio.service';
 import { AuthService } from 'src/app/services/auth.service';
@@ -14,28 +14,43 @@ import { Subject, takeUntil } from 'rxjs';
 export class RoomComponent implements OnInit, OnDestroy {
 
   public user: User | null = null;
-  public isOwner: boolean = false
+  public isHost: boolean = false
   public conversation: Conversation | null = null
-  public destroy$ = new Subject<void>()
+  private destroy$ = new Subject<void>()
+  private isJoined: boolean = false
+  public loading: boolean = true
 
   constructor(private twilioService: TwilioService,
               private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
-    this.twilioService.getConversation().pipe(takeUntil(this.destroy$)).subscribe(conversation => {
+    this.twilioService.getConversation().pipe(takeUntil(this.destroy$)).subscribe(async conversation => {
       this.conversation = conversation
+      if (this.isJoined || this.conversation?.status === 'joined') {
+        this.isJoined = true
+        this.loading = false
+        return;
+      }
+      const roomId = this.route.snapshot.paramMap.get('id')
+      if (roomId) await this.twilioService.joinRoom(roomId)
+      this.loading = false
     })
     this.authService.getUser().pipe(takeUntil(this.destroy$)).subscribe(user => {
       this.user = user
-      this.isOwner = this.user?.user_name === this.conversation?.createdBy
+      this.isHost = this.twilioService.isHost()
+    })
+    this.twilioService.onMessage.pipe(takeUntil(this.destroy$)).subscribe(message => {
+      console.log(message);
     })
   }
 
   ngOnDestroy(): void {
     this.destroy$.next()
     this.destroy$.complete()
-    //this.conversation?.removeAllListeners()
+    this.conversation?.removeAllListeners()
   }
 
   async sendMessage(message: string) {
