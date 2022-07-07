5f23d9bf6d70ebb43f35a58fca4b7183129b5369
Tomas Melone <60193559+tomimelo@users.noreply.github.com>
2022-07-07 10:21:34 +0000

    add base url env

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
+  baseUrl: 'https://discuter.vercel.app',
   supabaseUrl: 'https://qsewkuaclihjlpterjeq.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzZXdrdWFjbGloamxwdGVyamVxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTY3MTc0MzMsImV4cCI6MTk3MjI5MzQzM30.Y842Aa06USUjaq9oIDZ0JuQE453aOb96OdkIUgCgO8E',
   apiUrl: 'https://discuter-production.up.railway.app/api'
