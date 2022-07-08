7afe56228ebd7c943feb3c51f55a64d4e8f21834
Tomas Melone <tomasmelone@hotmail.com>
2022-07-09 00:25:06 +0200

    extend navbar functionality

--- frontend/src/app/shared/navbar/navbar.component.ts
+++ frontend/src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
 
+type NavbarFixedPositions = 'top';
+type NavbarAlignPositions = 'left' | 'center' | 'right';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -8,6 +10,28 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   constructor() { }
+
+  @Input() fixed: NavbarFixedPositions | boolean = false;
+  @HostBinding('class.fixed-top')
+  public get isFixedTop(): boolean {
+    return this.fixed === 'top' || this.fixed === true;
+  }
+
+  @Input() align!: NavbarAlignPositions;
+  @HostBinding('class.align-left')
+  public get isLeftAlign(): boolean {
+    return this.align === 'left';
+  }
+
+  @HostBinding('class.align-center')
+  public get isCenterAlign(): boolean {
+    return this.align === 'center';
+  }
+
+  @HostBinding('class.align-right')
+  public get isRightAlign(): boolean {
+    return this.align === 'right';
+  }
 
   ngOnInit(): void {
   }
