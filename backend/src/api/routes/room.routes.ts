ea1e6d84801fb70dcc6a747441a3eb4d987e8568
Tomas Melone <tomasmelone@hotmail.com>
2022-07-14 23:21:10 +0200

    use supabase service acqurier

--- backend/src/api/routes/room.routes.ts
+++ backend/src/api/routes/room.routes.ts
@@ -1,22 +1,16 @@
 import { MadRoute, MadRouteMethod, MadRouter } from 'mad-server'
 import { RoomController } from '../../room/room-controller'
-import { SupabaseConfig } from '../../supabase/supabase-config'
-import { SupabaseService } from '../../supabase/supabase-service'
 import { authServiceAcquirer } from '../../utils/acquirers/auth-service-acquirer'
 import { loggerAcquirer } from '../../utils/acquirers/logger-acquirer'
+import { supabaseServiceAcquirer } from '../../utils/acquirers/supabase-service-acquirer'
 import { twilioClientAcquirer } from '../../utils/acquirers/twilio-client-acquirer'
 import verifyJWT from '../middlewares/verifyJwt'
 
 const logger = loggerAcquirer.acquire().child('RoomController')
-const supabaseConfig: SupabaseConfig = {
-  url: process.env.SUPABASE_URL || '',
-  serviceRole: process.env.SUPABASE_SERVICE_ROLE || ''
-}
 
+const supabaseService = supabaseServiceAcquirer.acquire()
 const authService = authServiceAcquirer.acquire()
 const twilioClient = twilioClientAcquirer.acquire()
-
-const supabaseService = new SupabaseService(supabaseConfig)
 const roomController = new RoomController(supabaseService, twilioClient, logger)
 
 const getLink: MadRoute = {
