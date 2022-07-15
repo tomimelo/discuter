403bbd8880733b9760dcccf431b2e4ee6534753d
Tomas Melone <60193559+tomimelo@users.noreply.github.com>
2022-07-15 10:21:57 +0000

    scroll to bottom on new message

--- frontend/src/app/components/chat/chat.component.ts
+++ frontend/src/app/components/chat/chat.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, EventEmitter, Inject, Input, OnDestroy, OnInit, Output } from '@angular/core';
+import { filter, Subject, takeUntil } from 'rxjs';
+import { RoomService } from 'src/app/services/room.service';
 import { Message } from 'src/app/types/message';
 
 @Component({
@@ -6,18 +9,47 @@ import { Message } from 'src/app/types/message';
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, OnDestroy {
 
   @Input() messages: Message[] = [];
   @Output() onSend = new EventEmitter<string>();
+  private messagesContainer: HTMLElement | null = null;
+  private destroy$ = new Subject<void>()
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private roomService: RoomService,
+              @Inject(DOCUMENT) private document: Document) {
+  }
+  
+  public ngOnInit(): void {
+    this.getMessagesContainer()
+    this.onNewMessage();
+  }
+  
+  public ngOnDestroy(): void {
+    this.destroy$.next()
+    this.destroy$.complete()
+  }
+  
+  public sendMessage(message: string) {
+    this.onSend.emit(message);
   }
 
-  sendMessage(message: string) {
-    this.onSend.emit(message);
+  private getMessagesContainer(): void {
+    this.messagesContainer = this.document.getElementById('messages')
+  }
+
+  private onNewMessage() {
+    this.roomService.onChanges.pipe(filter(event => event.type === 'messageAdded'), takeUntil(this.destroy$)).subscribe(() => {
+      setTimeout(() => {
+        this.scrollToBottom()
+      }, 100)
+    })
+  }
+
+  private scrollToBottom() {
+    if (this.messagesContainer) {
+      this.messagesContainer.scrollTop = this.messagesContainer.scrollHeight;
+    }
   }
 
 }
