e6a9b93c7823b22ade92126d0799883cc2cafe7b
Tomas Melone <tomasmelone@hotmail.com>
2022-07-17 00:22:28 +0200

    set invite page styles

--- frontend/src/app/pages/invite/invite.module.ts
+++ frontend/src/app/pages/invite/invite.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { InviteRoutingModule } from './invite-routing.module';
 import { InviteComponent } from './invite.component';
+import { TuiLoaderModule } from '@taiga-ui/core';
 
-
+const TAIGA_MODULES = [
+  TuiLoaderModule
+]
 @NgModule({
   declarations: [
     InviteComponent
   ],
   imports: [
     CommonModule,
-    InviteRoutingModule
+    InviteRoutingModule,
+    ...TAIGA_MODULES
   ]
 })
 export class InviteModule { }
