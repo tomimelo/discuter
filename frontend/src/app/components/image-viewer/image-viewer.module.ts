01ebf7d66a37ab89b7a7ae3b7ba6345e2d8df775
Tomas Melone <tomasmelone@hotmail.com>
2022-07-21 20:06:19 +0200

    show images

--- frontend/src/app/components/image-viewer/image-viewer.module.ts
+++ frontend/src/app/components/image-viewer/image-viewer.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImageViewerComponent } from './image-viewer.component';
+import { TuiLoaderModule } from '@taiga-ui/core';
+
+const TAIGA_MODULES = [
+  TuiLoaderModule
+]
 
 @NgModule({
   declarations: [
     ImageViewerComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ...TAIGA_MODULES
   ],
   exports: [
     ImageViewerComponent
